01f97a2badfa37099d95867af947cace3aa76f94
minarul24 <minarul@uvic.ca>
2021-07-13 19:48:27 +0600

    for mean

--- main.m
+++ main.m
@@ -13,3 +13,14 @@ D_te = D_wbdc(:,286:569);
 y_tr = D_tr(31,:);
 y_te = D_te(31,:);
 
+X_tr = zeros(30,285);
+%for mean
+m = zeros(1,30);
+v = zeros(1,30);
+
+for i = 1:30
+    x_i = D_tr(i,:);
+    m(i) = mean(x_i);
+    v(i) = sqrt(var(x_i));
+    X_tr(i,:) = (x_i - m(i))/v(i);
+end
